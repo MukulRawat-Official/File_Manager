43c062ecea4ceb65f92db699cdf185f7711fd8a9
MukulRawat-Official <mukulrawat24032001@gmail.com>
2023-07-31 14:02:38 +0530

    Hash Function updated

---
+++ FolderSelectionControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace UI
+{
+    internal class FolderSelectionControl : UIElement
+    {
+        private Action<object> value;
+
+        public FolderSelectionControl(Action<object> value)
+        {
+            this.value = value;
+        }
+    }
+}
